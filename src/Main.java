0a830577b3e8e4edcca8fe6b3bb8be1e33d558e9
Мария <git@github.com:MariyaRass/helloworld.git>
2024-01-22 23:14:11 +0300

    Домашнее задание2

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        System.out.println("Задание 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+        System.out.println("Задание 3");
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        System.out.println("Задание 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        System.out.println("Задание 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        System.out.println("Задание 6");
+        var massOne = 78.2;
+        System.out.println(massOne);
+        var massTwo = 82.7;
+        System.out.println(massTwo);
+        var totalWeight = massOne + massTwo;
+        System.out.println("Масса двух бойцов " + totalWeight + " кг");
+        var difference = massTwo - massOne;
+        System.out.println("Разница между массами бойцов " + difference + " кг" );
+
+        System.out.println("Задание 7");
+        var massDifference = massTwo - massOne;
+        System.out.println("Разница масс спортсменов " + massDifference + " кг" );
+        massDifference = massTwo % massOne;
+        System.out.println("Разница масс спортсменов " + massDifference + " кг" );
+
+        System.out.println("Задание 8");
+        var brumalesHoras = 640;
+        System.out.println (brumalesHoras);
+        var unusOperarius = 8;
+        System.out.println (unusOperarius);
+        var totalisConductos = brumalesHoras / unusOperarius;
+        System.out.println("Всего работников в компании — " + totalisConductos + " человек" );
+        totalisConductos = totalisConductos + 94;
+        System.out.println (totalisConductos);
+        var totalishoris = unusOperarius * totalisConductos;
+        System.out.println ("Если в компании работает " + totalisConductos + " человека, то всего " + totalishoris + " часа работы может быть поделено между сотрудниками");
+
+
+
+
+
+    }}
+
